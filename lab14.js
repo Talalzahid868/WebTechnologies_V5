a15070e45f1e4f48a8c5858e21cfe83a19d936d9
Talal Zahid <talalzahid8031@gmail.com>
2026-01-17 12:53:25 +0500

    Add files via upload

---
+++ lab14.js
@@ -0,0 +1,19 @@
+console.log("testing")
+
+
+const age ="5" +"4"
+console.log(age)
+const ae ="5" +4
+console.log(ae)
+
+console.log("pie"[2])
+console.log("pie"[3])
+
+
+let a=45
+console.log("After many yaers",a)
+console.log(`After many yaers we are ${a}`)
+
+let arr=[true,NaN,null,"687"]
+console.log(arr)
+console.log(arr.values())
